ed12bffad7c31f18bc4c3eb4e656b0460e0b6fa6
wilzson <1041935459@qq.com>
2026-02-14 13:04:25 +0800

    init

---
+++ src/main/java/org/example/springframeworkdemo/SpringFrameworkDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.springframeworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springframeworkdemo/SpringFrameworkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springframeworkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
